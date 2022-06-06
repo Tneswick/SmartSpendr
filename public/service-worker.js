da440903ab96f61f5f975210400d291f59e2bbf7
Deck Stream <tneswick2@verizon.net>
2022-06-06 09:55:48 -0700

    manifest completed

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,13 @@
-const APP_PREFIX = 'FoodEvent-';     
+const APP_PREFIX = 'SmartSpendr-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
   "./index.html",
-  "./events.html",
-  "./tickets.html",
-  "./schedule.html",
-  "./assets/css/style.css",
-  "./assets/css/bootstrap.css",
-  "./assets/css/tickets.css",
-  "./dist/app.bundle.js",
-  "./dist/events.bundle.js",
-  "./dist/tickets.bundle.js",
-  "./dist/schedule.bundle.js"
+  "./css/style.css",
+  "./js/index.js",
+  "./js/idb.js"
 ];
+// ============================================================================================
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
